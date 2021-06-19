c36c625a7e0b150e1d51898b01a434fec3393931
Cathy Richardson <cathyanne08@gmail.com>
2021-06-19 16:26:56 -0600

    previous and next buttons work

--- src/App.js
+++ src/App.js
@@ -2,43 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 import data from './data';
 import React from 'react';
+import User from './User';
 
 class App extends React.Component {
   constructor() {
     super()
-    console.log(data[0].name.first)
     this.state = {
-
+      currentUser: 0, //index of user to display 
     }
   }
+
+  handleClickNext = () => {
+    if (this.state.currentUser < data.length - 1) {
+      this.setState({ currentUser: this.state.currentUser + 1 })
+    }
+  }
+
+  handleClickPrevious = () => {
+    if (this.state.currentUser > 0) {
+      this.setState({ currentUser: this.state.currentUser - 1 })
+    }
+  }
+
 
   render() {
     return (
       <div className="App">
         <nav>Home</nav>
         <div className="background">
-          <section>
-            <h2>{`${data[0].id}/${data.length}`}</h2>
-            <h1>{`${data[0].name.first} ${data[0].name.last}`}</h1>
-            <br />
-            <p> <span className="bold">From:</span> {`${data[0].city}, ${data[0].country}`}</p>
-            <p> <span className="bold">Job Title:</span> {data[0].title} </p>
-            <p> <span className="bold">Employer:</span> {data[0].employer} </p>
-            <br />
-            <p> <span className="bold">Favorite Movies:</span>
-              <ol>
-                {data[0].favoriteMovies.map((e, i) => <li key={i}> {e} </li>)}
-              </ol>
-            </p>
-          </section>
+          <User user={data[this.state.currentUser]} totalUsers={data.length} />
           <footer>
-            <a className="previous"> &lt; Previous</a>
+            <a className="previous" onClick={this.handleClickPrevious}> &lt; Previous</a>
             <div className="buttons">
               <button>Edit</button>
               <button>Delete</button>
               <button>New</button>
             </div>
-            <a className="next" > Next &gt; </a>
+            <a className="next" onClick={this.handleClickNext}> Next &gt; </a>
           </footer>
         </div>
       </div>
---
+++ src/User.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function User(props) {
+    return (
+        <section>
+            <h2>{`${props.user.id}/${props.totalUsers}`}</h2>
+            <h1>{`${props.user.name.first} ${props.user.name.last}`}</h1>
+            <br />
+            <p> <span className="bold">From:</span> {`${props.user.city}, ${props.user.country}`}</p>
+            <p> <span className="bold">Job Title:</span> {props.user.title} </p>
+            <p> <span className="bold">Employer:</span> {props.user.employer} </p>
+            <br />
+            <p> <span className="bold">Favorite Movies:</span>
+                <ol>
+                    {props.user.favoriteMovies.map((e, i) => <li key={i}> {e} </li>)}
+                </ol>
+            </p>
+        </section>
+    )
+}
